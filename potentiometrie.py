741cad5279c5db367ab1a263eb77d071fa6c758d
Gustav267 <gustav.brauer@web.de>
2026-02-27 14:07:48 +0100

    Update potentiometrie.py

--- potentiometrie.py
+++ potentiometrie.py
@@ -7,45 +7,28 @@ from scipy.optimize import root_scalar
 import scipy.optimize as opt
 
 i=0
-# Anzahl der Iterationen für den Fit (je mehr, desto genauer,maximal 10^10)
+# Anzahl der Iterationen für den Fit 
 iterationen = 100000000
-# Excel-Datei einlesen(möglichst viele und dichte Messpunkte, mindestens 9)
-df = pd.read_excel("\\Potentiometrie-all-data.xlsx")
+# Excel-Datei einlesen(möglichst viele und dichte Messpunkte)
+df = pd.read_excel("C:\\Users\\Gusta\\Documents\\Jugend forscht\\Datensaetze\\Potentiometrie-all-data.xlsx")
 vol_naoh, ph_wert = df.iloc[5:, i*2].dropna(), df.iloc[5:, i*2+1].dropna()
 cta=("%s%s" % (str(df.iloc[2, (i*2)+1]),df.iloc[3, (i*2)+1]))
 print(f'CTA: {cta}, i: {i}')
-# Drop all pH values strictly between 4 and 10 and their corresponding vol_naoh
-# Keep only points where pH <= 4 or pH >= 10
 
 
-# Berechnung des Startwerts für C --> mittelwert der x-Werte an der größten Steigung
-max_diff_index = np.argmax(np.diff(ph_wert))
-# Aep_gesch ist C_start skaliert auf die Original-Volumenskala
-Aep_gesch = np.mean(vol_naoh[max_diff_index:max_diff_index + 2])
-
-# Erste Filterung: Drop all vol_naoh values strictly between Aep_gesch - 5 and Aep_gesch + 5 and their corresponding ph_wert
-# Keep only points where vol_naoh <= Aep_gesch - 5 or vol_naoh >= Aep_gesch + 5
-
-
-
+# feststellung der min und max Werte für die x-Achse (Volumen NaOH)
 vol_max, vol_min = max(vol_naoh), min(vol_naoh)
-# Funktion zur Rücktransformation der skalierten x-Werte
-def rescale_x(x):
-    return (x)
-# Funktion zur Skalierung der x-Werte auf [0, 1] normieren
-def skaliere_x(x):
-    return (x)
-x_scaled = skaliere_x(vol_naoh)
+
 
 ##Definition der Funktionen##
 
-# Definition der 7-Parameter-Logistischen Funktion (PL7)
-def seven_pl(x, A, D, C, B, G, F):
+# Definition der Funktion (PL7)
+def funktion(x, A, D, C, B, G, F):
     return (D + (A - D) / ((1 + ( ((x / C) ** B)) ** G))+ (F * x))
-# Erste ableitung der 7-Parameter-Logistischen Funktion  (skaliert)
+# Erste ableitung der Funktion
 def erst_abl(x, A, D, C, B, G, F):
     return (-B*G*(A - D)*((x/C)**B)**G/(x*(((x/C)**B)**G + 1)**2) + F)
-# Zweite Ableitung der 7-Parameter-Logistischen Funktion
+# Zweite Ableitung der Funktion
 def zwe_abl(x, A, D, C, B, G,F):
     return -2*B**2*G**2*(x/C)**(B*G)*(x/C)**(B*G - 1)*(A - D)/(C*x*((x/C)**(B*G) + 1)**3) + B*G*(x/C)**(B*G - 1)*(A - D)*(B*G - 1)/(C*x*((x/C)**(B*G) + 1)**2)
 ##FIT ESRSTELLUNG ###
@@ -53,8 +36,9 @@ def zwe_abl(x, A, D, C, B, G,F):
 # Verbesserte Schätzung der Parameterwerte basierend auf skalierten Daten
 D_start, A_start = max(ph_wert), min(ph_wert)
 max_diff_index = np.argmax(np.diff(ph_wert))
-##Berechnung des Startwerts für C--> mittelwert der x-Werte an der größten Steigung
-C_start = skaliere_x(Aep_gesch)
+# Berechnung des Startwerts für C --> mittelwert der x-Werte an der größten Steigung. Korreliert mit dem Äquivalenzpunkt.
+max_diff_index = np.argmax(np.diff(ph_wert))
+C_start = np.mean(vol_naoh[max_diff_index:max_diff_index + 2])
 
 
 param_bounds = {
@@ -65,41 +49,35 @@ param_bounds = {
     "F": (-1, 2),
     }
 
-# Gewichte für den Fit kp,ob hier optimal) 
-#weights =1+0.0001 * (1/np.abs(x_scaled - C_start + 1e-10)) # Vermeidet Division durch Null 
+
 # lmfit-Modell erstellen und Parametergrenzen setzen
-model = Model(seven_pl)
+model = Model(funktion)
 params = model.make_params(A=A_start, D=D_start, C=C_start, B=10, G=1, F=1)
 for param, (lower, upper) in param_bounds.items():
     params[param].set(min=lower, max=upper)
 
 # Fit durchführen mit skalierten x-Werten
-result = model.fit(ph_wert, params, x=x_scaled, method="leastsq", max_nfev=iterationen)
+result = model.fit(ph_wert, params, x=vol_naoh, method="leastsq", max_nfev=iterationen)
 print(result.fit_report())
 print(f'Anzahl der durchgeführten Iterationen: {result.nfev}')
 ##Auswertung der Fit Ergebnisse##
 
 # Angepassten C Parameter extrahieren--> startwert für die Suche nach AQ punkt
 C_start = result.params['C'].value
-Aep_gesch= rescale_x(C_start)
-print(f'Parameter C (Aep_gesch) bei: {Aep_gesch:.3f} ml')
+
+print(f'Parameter C (C_start) bei: {C_start:.3f} ml')
 # Extract R² from the fit report and save it as a parameter
 r_squared_from_report = result.rsquared if hasattr(result, 'rsquared') else None
 
-## Tangenten- und pH-Verfahren entfernt. Auswertung erfolgt nur über
-## die Nullstelle der zweiten Ableitung der Fit-Kurve.
+
 
 ### Nullstelle Zweiter Ableitung###
 
-# Zweite Ableitung der Fit-Kurve berechnen und Nullstellen suchen
-# Suche im gesamten Messbereich (vol_min, vol_max) mit Brentq
-x_scaled_nullstellen_zweiter_ableitung = root_scalar(
+x_nullstellen_zweiter_ableitung = root_scalar(
     lambda x: zwe_abl(x, **result.best_values),
-    bracket=(vol_min+1, vol_max-1),
-    method='brentq'
+    x0=C_start,
+    method='newton'
 ).root
-vol_nullstellen_zweiter_ableitung = rescale_x(x_scaled_nullstellen_zweiter_ableitung)
-
 
 ## Plot der Titrationskurve und Ableitung##
 font = {
@@ -108,11 +86,11 @@ font = {
 mpl.rc('font', **font)
 cm= 1 / 2.54
 # Fit-Kurve berechnen für grafische Darstellung
-x_linespace_scaled = np.linspace(vol_min, vol_max, num=200)
-ph_zu_linespace = seven_pl(x_linespace_scaled, **result.best_values)
-x_linespace_ml = rescale_x(x_linespace_scaled)
-#ph_fit_deriv = erst_abl(x_linespace_scaled, **result.best_values)
-#ph_fit_deriv2 = zwe_abl(x_linespace_scaled, **result.best_values)
+ml_linespace = np.linspace(vol_min, vol_max, num=200)
+ph_zu_linespace = funktion(ml_linespace, **result.best_values)
+
+#ph_fit_deriv = erst_abl(ml_linespace, **result.best_values)
+#ph_fit_deriv2 = zwe_abl(ml_linespace, **result.best_values)
 # Plot der Titrationskurve und Ableitung
 fig, ax1 = plt.subplots(figsize=(16*cm,12*cm))
 #ax2 = ax1.twinx()
@@ -121,14 +99,14 @@ fig, ax1 = plt.subplots(figsize=(16*cm,12*cm))
 #ax1.patch.set_visible(False)
 
 ax1.scatter(vol_naoh, ph_wert, color='blue', label='Messpunkte', marker='x', zorder=6, s=20)
-ax1.plot(x_linespace_ml, ph_zu_linespace, color='orange', label='Regression', linewidth=1.2, zorder=4)
-#ax2.plot(x_linespace_ml, ph_fit_deriv, color='green', label='1. Ableitung', linewidth=0.5, linestyle='dashed', zorder=1)
-#ax2.plot(x_linespace_ml, ph_fit_deriv2, color='gray', label='2. Ableitung', linewidth=0.5, linestyle=':', zorder=1)
+ax1.plot(ml_linespace, ph_zu_linespace, color='orange', label='Regression', linewidth=1.2, zorder=4)
+#ax2.plot(ml_linespace, ph_fit_deriv, color='green', label='1. Ableitung', linewidth=0.5, linestyle='dashed', zorder=1)
+#ax2.plot(ml_linespace, ph_fit_deriv2, color='gray', label='2. Ableitung', linewidth=0.5, linestyle=':', zorder=1)
 
 #ax2.scatter([vol_naoh], [weights], color='green', label='gewichtung', marker='_', zorder=3, s=20)
-#ax1.scatter(Aep_gesch,seven_pl(C_start,**result.best_values) , color='gray', label=f'Parameter C bei {Aep_gesch:.3f} ml', marker='_', zorder=1, s=20)
+#ax1.scatter(C_start,funktion(C_start,**result.best_values) , color='gray', label=f'Parameter C bei {C_start:.3f} ml', marker='_', zorder=1, s=20)
 #ax1.scatter(x_ph7, 7, color='green', label=f'f(x)=7 bei {x_ph7:.1f} ml', marker='_', zorder=7, s=20)
-ax1.scatter(vol_nullstellen_zweiter_ableitung, seven_pl(x_scaled_nullstellen_zweiter_ableitung, **result.best_values), color='purple', label=f'f\'\'(x)=0 bei {vol_nullstellen_zweiter_ableitung:.1f} ml', marker='_', zorder=9, s=20)  
+ax1.scatter(x_nullstellen_zweiter_ableitung, funktion(x_nullstellen_zweiter_ableitung, **result.best_values), color='purple', label=f'f\'\'(x)=0 bei {x_nullstellen_zweiter_ableitung:.1f} ml', marker='_', zorder=9, s=20)  
 
 
 # Hinzufügen von R² zur Legende
